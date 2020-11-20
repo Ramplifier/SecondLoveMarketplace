c9279faa442c005f3e58c7b2a256ff2e56eded46
Ramplifier <62212424+Ramplifier@users.noreply.github.com>
2020-11-20 18:17:22 +0100

    front-page styling

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,20 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+// @ts-ignore
+import {BrowserModule} from '@angular/platform-browser';
+// @ts-ignore
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import { BannerComponent } from './components/banner/banner.component';
+import { MenuComponent } from './components/menuComponents/menu/menu.component';
+import { MenuButtonComponent } from './components/menuComponents/menu-button/menu-button.component';
 
+// @ts-ignore
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BannerComponent,
+    MenuComponent,
+    MenuButtonComponent,
   ],
   imports: [
     BrowserModule
---
+++ src/app/components/menuComponents/menu-button/menu-button.component.ts
@@ -0,0 +1,17 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'menu-button',
+  templateUrl: './menu-button.component.html',
+  styleUrls: ['./menu-button.component.css']
+})
+export class MenuButtonComponent implements OnInit {
+  naam = 'product';
+
+  constructor() {
+  }
+
+  ngOnInit(): void {
+  }
+
+}
